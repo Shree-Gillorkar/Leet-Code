9f7923ea515ba5939e2b902cca65845a10d78f5a
Shree Gillorkar <70030205+Shree-Gillorkar@users.noreply.github.com>
2021-07-07 09:02:11 +0530

    Create Fibonacci Number.java

---
+++ Fibonacci Number.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int fib(int n) {
+          if(n == 0)
+            return 0;
+        if(n == 1)
+            return 1;
+        if(n == 2)
+            return 1;
+        int f0 = 1;
+        int f1 = 1;
+        int fn = 0;
+        for(int i = 2; i<n; i++){
+            fn = f1 + f0;
+            f0 = f1;
+            f1 = fn;
+        }
+        return fn;
+    }
+}
