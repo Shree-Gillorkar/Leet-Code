4442aaf8182bceb6fad11d5605ae0fef3cb72948
Shree Gillorkar <70030205+Shree-Gillorkar@users.noreply.github.com>
2021-12-09 23:36:18 +0530

    Create Jump Game III.java

---
+++ Jump Game III.java
@@ -0,0 +1,58 @@
+/*
+Given an array of non-negative integers arr, you are initially positioned at start index of the array. When you are at index i, you can jump to i + arr[i] or i - arr[i], check if you can reach to any index with value 0.
+Notice that you can not jump outside of the array at any time.
+
+Example 1:
+Input: arr = [4,2,3,0,3,1,2], start = 5
+Output: true
+Explanation: 
+All possible ways to reach at index 3 with value 0 are: 
+index 5 -> index 4 -> index 1 -> index 3 
+index 5 -> index 6 -> index 4 -> index 1 -> index 3 
+
+Example 2:
+Input: arr = [4,2,3,0,3,1,2], start = 0
+Output: true 
+Explanation: 
+One possible way to reach at index 3 with value 0 is: 
+index 0 -> index 4 -> index 1 -> index 3
+
+Example 3:
+Input: arr = [3,0,2,1,2], start = 2
+Output: false
+Explanation: There is no way to reach at index 1 with value 0.
+ 
+
+Constraints:
+1 <= arr.length <= 5 * 104
+0 <= arr[i] < arr.length
+0 <= start < arr.length
+*/
+
+
+class Solution {
+    public boolean canReach(int[] arr, int start) {
+        Queue<Integer> q = new LinkedList<>();
+    q.add(start);
+    
+    while(!q.isEmpty()){
+        
+        int idx = q.poll();
+        
+        if(arr[idx]==0)
+            return true;
+        
+        int r = idx+arr[idx];
+        int l = idx-arr[idx];
+        
+        arr[idx] = -1; 
+        
+        if(r<arr.length && arr[r]!=-1)
+            q.add(r);
+        if(l>=0 && arr[l]!=-1)
+            q.add(l);
+        
+    }
+    return false;
+    }
+}
